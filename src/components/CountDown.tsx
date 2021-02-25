e4625bfffb7514fe9504ef8b332284183218bc52
matheus <matheus.sa.alves@outlook.com>
2021-02-24 21:49:30 -0300

    Finalizando estilizações e iniciando com ContextAPI

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,11 +1,17 @@
 import styles from '../styles/components/CountDown.module.css';
 
 // HOOKS
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+let countDownTimeOut: NodeJS.Timeout; //Isso aqui serve pra gente limpar o delay do setTimeOut continuação na linha 29
 
 export function CountDown() {
-  const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.05 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time) / 60;
   const seconds = time % 60;
@@ -14,16 +20,27 @@ export function CountDown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   function startCountDown() {
-    setActive(true);
+    setIsActive(true);
+  }
+
+  function resetCountDown() {
+    clearTimeout(countDownTimeOut); // essa função clearTimeout reseta o delay do setTimeout
+    setIsActive(false);
+    setTime(0.05 * 60);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (isActive && time > 0) {
+      countDownTimeOut = setTimeout(() => {
+        // Agente chama o setTimeout como valor do countDownTimeOut e reseta ele na linha 23
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -39,13 +56,44 @@ export function CountDown() {
         </div>
       </div>
 
-      <button
-        type='button'
-        className={styles.countDownButton}
-        onClick={startCountDown}
-      >
-        Iniciar um ciclo
-      </button>
+      {hasFinished ? (
+        <button disabled className={styles.countDownButton}>
+          Ciclo encerrado
+          <img
+            src='/icons/check.svg'
+            alt='Um simbolo de check marcando que a contagem chegou a 0 e o objetivo foi concluido'
+          />
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type='button'
+              className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
+              onClick={resetCountDown}
+            >
+              Abandonar ciclo
+              <img
+                src='/icons/close-cicle.svg'
+                alt='um X indicando o final da contagem'
+                style={{ filter: 'invert(1)' }}
+              />
+            </button>
+          ) : (
+            <button
+              type='button'
+              className={styles.countDownButton}
+              onClick={startCountDown}
+            >
+              Iniciar um ciclo
+              <img
+                src='/icons/arrow.svg'
+                alt='Uma seta apontada para a direita indicando o Inicio da contagem'
+              />
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 // STYLES
 import styles from '../styles/pages/Home.module.css';
 
@@ -7,6 +8,7 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { CountDown } from '../components/CountDown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -22,7 +24,9 @@ export default function Home() {
           <CompletedChallenges />
           <CountDown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
